946e582de5b4b27150f07d09a2af6e92512c6188
Jackson <brandon.jackson@optum.com>
2019-11-08 18:50:22 -0600

    Second Commit

---
+++ CharAndBoolean/src/academy/learnprogramming/Main.java
@@ -0,0 +1,19 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char myUnicodeChar2 = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myUnicodeChar2);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
